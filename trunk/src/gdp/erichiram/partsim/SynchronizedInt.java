47733b9164cb2a770b6e93d340adb9d7ba1b96c9
hiramvanpaassen <hiramvanpaassen@364f37a4-301b-11de-b3e3-fb1b389a3697>
2009-05-04 13:29:24 +0000

    p en pMax werken nu en de threadpool is aan te passen

--- trunk/src/gdp/erichiram/partsim/SynchronizedInt.java
+++ trunk/src/gdp/erichiram/partsim/SynchronizedInt.java
@@ -2,4 +2,39 @@ package gdp.erichiram.partsim;
 
 public class SynchronizedInt {
 
+	private int i;
+
+	public synchronized int decrease()
+	{
+		return --i;
+	}
+	
+	public synchronized int increase()
+	{
+		return ++i;
+	}
+	
+	public synchronized int get()
+	{
+		return i;
+	}
+	
+	public synchronized int testIncrAndDo(TestIncrAndDo tiad){
+		if (tiad.test(i))
+		{
+			increase();
+			tiad.exec();
+		}
+		return i;
+	}
+		
+}
+
+
+abstract class TestIncrAndDo {
+
+	public abstract boolean test(int i);
+
+	public abstract void exec();
+	
 }
