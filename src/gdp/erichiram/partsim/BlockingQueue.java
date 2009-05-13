9868cd1b037813f58d52ba14b0d9e004d3bd1db9
hiramvanpaassen <hmvp@users.noreply.github.com>
2009-05-13 10:22:40 +0000

    fix roundnumbers on added particles

---
+++ src/gdp/erichiram/partsim/BlockingQueue.java
@@ -0,0 +1,32 @@
+package gdp.erichiram.partsim;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BlockingQueue<T> {
+	private Queue<T> q = new LinkedList<T>();
+	private int polls = 0;
+
+	public synchronized void addAll(Collection<T> c) {
+		q.addAll(c);
+	}
+
+	public synchronized int size() {
+		return q.size();
+	}
+
+	public synchronized void offer(T t) {
+		q.offer(t);		
+	}
+
+	public synchronized T poll() {
+		
+		return q.poll();
+	}
+
+	public synchronized T peek() {
+		return q.peek();
+	}
+
+}
