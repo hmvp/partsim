28ce6b1b5add7ad6eba9e9ac179b0ecd9d9430f6
hiramvanpaassen <hmvp@users.noreply.github.com>
2009-05-18 09:36:22 +0000

    Threadpool comments and removal of periodic wakeup

--- src/gdp/erichiram/partsim/ThreadPool.java
+++ src/gdp/erichiram/partsim/ThreadPool.java
@@ -10,36 +10,56 @@ import java.util.LinkedList;
 
 public class ThreadPool extends Thread {
 
+	/**
+	 * collection of animation threads
+	 */
 	private Collection<Animation> pool = new LinkedList<Animation>();
+	
+	/**
+	 * maximum number of animation threads in this pool
+	 */
 	private int pMax = 0;
 
+	/**
+	 * reference to main program used to pass on to animation threads
+	 */
 	private Main main;
 
+	/**
+	 * default constructor
+	 * @param main reference to main
+	 */
 	public ThreadPool(Main main) {
 		this.main = main;
-		this.start();
+		start();
 	}
 
+	/** 
+	 * set the maximum number of threads
+	 * @param i the maximum number of threads
+	 */
 	public synchronized void setPmax(int i) {
 		Main.debug("Pmax changed to:" + i);
 		pMax = i;
 		notify();
 	}
 
+	/**
+	 * update threadpool, start new threads if necessary or stop them if necessary
+	 */
 	public synchronized void run() {
 		while (true) {
 			
 			int order = pMax - pool.size();
 
 			if (order <= 0) {
-				Main.debug("update p, decrease order: " + order);
-				for (Iterator<Animation> iter = pool.iterator(); order < 0
-						&& iter.hasNext(); order++) {
+				Main.debug("update p, decrease threads: " + order);
+				for (Iterator<Animation> iter = pool.iterator(); order < 0 && iter.hasNext(); order++) {
 					iter.next().finish();
 				}
 
 			} else {
-				Main.debug("update p, increase order: " + order);
+				Main.debug("update p, increase threads: " + order);
 				for (; order > 0 && pool.size() < main.getParticles().size(); order--) {
 					Animation a = new Animation(main);
 					pool.add(a);
@@ -47,7 +67,7 @@ public class ThreadPool extends Thread {
 				}
 			}
 			try {
-				wait(1000);
+				wait();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
@@ -55,14 +75,32 @@ public class ThreadPool extends Thread {
 		}
 	}
 
-	public synchronized void removeThread(Animation t) {
+	/**
+	 * remove a thread from the pool, only used by animation threads who need to stop
+	 * @param t
+	 */
+	protected synchronized void removeThread(Animation t) {
 		Main.debug("thread removes itself, thread: " + t.getId());
 		if (!pool.remove(t)) {
 			throw new RuntimeException("Thread is niet in pool!!!!");
 		}
 	}
 
+	/**
+	 * Get the number of threads in the pool used for painting that number
+	 * @return number of threads
+	 */
 	public synchronized int size() {
 		return pool.size();
+	}
+	
+	
+	/**
+	 * geef andere code de gelegenheid om de threadpool thread wakker te maken. 
+	 * Bijvoorbeeld als er een particle is toegevoegd en er wellicht een thread bij mag.
+	 */
+	public synchronized void update()
+	{
+		notify();
 	}
 }
