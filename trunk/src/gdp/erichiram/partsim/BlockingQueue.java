7f26a3e09c6c7bd502598cb6653640b5ff577654
eric.broersma <eric.broersma@364f37a4-301b-11de-b3e3-fb1b389a3697>
2009-05-18 10:05:43 +0000

    replaced java.util.concurrent.LinkedBlockingQueue with BlockingQueue

--- trunk/src/gdp/erichiram/partsim/BlockingQueue.java
+++ trunk/src/gdp/erichiram/partsim/BlockingQueue.java
@@ -5,23 +5,23 @@
 package gdp.erichiram.partsim;
 
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class BlockingQueue<T> {
+public class BlockingQueue<T> implements Queue<T> {
 	private Queue<T> q = new LinkedList<T>();
-	private int polls = 0;
 
-	public synchronized void addAll(Collection<T> c) {
-		q.addAll(c);
+	public synchronized boolean addAll(Collection<? extends T> c) {
+		return q.addAll(c);
 	}
 
 	public synchronized int size() {
 		return q.size();
 	}
 
-	public synchronized void offer(T t) {
-		q.offer(t);		
+	public synchronized boolean offer(T t) {
+		return q.offer(t);
 	}
 
 	public synchronized T poll() {
@@ -33,4 +33,65 @@ public class BlockingQueue<T> {
 		return q.peek();
 	}
 
+	public T element() {
+		throw new UnsupportedOperationException();
+	}
+
+	public T remove() {
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean add(T o) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void clear() {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	public boolean contains(Object o) {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	public boolean containsAll(Collection<?> c) {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	public boolean isEmpty() {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	public Iterator<T> iterator() {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	public boolean remove(Object o) {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	public boolean removeAll(Collection<?> c) {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	public boolean retainAll(Collection<?> c) {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	public Object[] toArray() {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	public <U> U[] toArray(U[] a) {
+		throw new UnsupportedOperationException();
+		
+	}
 }
