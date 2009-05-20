829951d067a60f14d09451ba2875555765c41414
eric.broersma <eric.broersma@364f37a4-301b-11de-b3e3-fb1b389a3697>
2009-05-20 11:56:47 +0000

    Particle input file reset to default. Added stupidMove (using a loop) to Particle. Using Particle.stupidMove in Animation. Added cool new unit tests to ParticleTest.

--- trunk/src/gdp/erichiram/partsim/test/ParticleTest.java
+++ trunk/src/gdp/erichiram/partsim/test/ParticleTest.java
@@ -4,6 +4,7 @@
 
 package gdp.erichiram.partsim.test;
 
+import gdp.erichiram.partsim.Main;
 import gdp.erichiram.partsim.Particle;
 
 import org.junit.Assert;
@@ -16,7 +17,7 @@ public class ParticleTest {
 	@Test
 	public void testRandomParticle() {
 		
-		for (int i = 0; i < 100000; i++ ) {
+		for (int i = 0; i < 10000; i++ ) {
 			Particle p = new Particle(i);
 			Assert.assertTrue(p.getDx() >= -799 && p.getDx() <= 799 );
 			Assert.assertTrue(p.getDy() >= -599 && p.getDy() <= 599 );
@@ -28,21 +29,158 @@ public class ParticleTest {
 		
 		for (int i = 0; i < 1000; i++ ) {
 			Particle p = new Particle(i);
-			for ( int j = 0; j < 10000; j++ ) {
+			for ( int j = 0; j < 100; j++ ) {
 				p.move();
 				Assert.assertTrue(p.getX() >= 0 && p.getX() < 800 );
 				Assert.assertTrue(p.getY() >= 0 && p.getY() < 600 );
 			}
 		}
+	}
+	
+	@Test
+	public void testParticleStupidMove() {
+		
+		for (int i = 0; i < 1000; i++ ) {
+			Particle p = new Particle(i);
+			for ( int j = 0; j < 100; j++ ) {
+				p.stupidMove();
+				Assert.assertTrue(p.getX() >= 0 && p.getX() < 800 );
+				Assert.assertTrue(p.getY() >= 0 && p.getY() < 600 );
+			}
+		}
+	}
+	
+	@Test
+	public void testParticleBigDMove() {
 		
 		for (int i = 0; i < 1000; i++ ) {
 			Particle p = new Particle(i);
 			p.setDx((int) (Math.random() * 10000 - 5000));
 			p.setDy((int) (Math.random() * 10000 - 5000));
-			for ( int j = 0; j < 10000; j++ ) {
+			for ( int j = 0; j < 100; j++ ) {
 				p.move();
 				Assert.assertTrue(p.getX() >= 0 && p.getX() < 800 );
 				Assert.assertTrue(p.getY() >= 0 && p.getY() < 600 );
+			}
+		}
+	}
+	
+	@Test
+	public void testParticleBigDStupidMove() {
+		
+		for (int i = 0; i < 1000; i++ ) {
+			Particle p = new Particle(i);
+			p.setDx((int) (Math.random() * 10000 - 5000));
+			p.setDy((int) (Math.random() * 10000 - 5000));
+			for ( int j = 0; j < 100; j++ ) {
+				p.stupidMove();
+				Assert.assertTrue(p.getX() >= 0 && p.getX() < 800 );
+				Assert.assertTrue(p.getY() >= 0 && p.getY() < 600 );
+			}
+		}
+	}
+	
+	@Test
+	public void testParticleStaticMove() {
+		
+		Particle p = new Particle(799,599,-800,-600,'a', 0);
+		p.move();
+		Assert.assertEquals(1, p.getX());
+		Assert.assertEquals(1, p.getY());
+		
+		p = new Particle(0,0,800,600,'a', 0);
+		p.move();
+		Assert.assertEquals(798, p.getX());
+		Assert.assertEquals(598, p.getY());
+		
+		p = new Particle(799,599,1598,1198,'a', 0);
+		p.move();
+		Assert.assertEquals(799, p.getX());
+		Assert.assertEquals(599, p.getY());
+		
+		p = new Particle(0,0,1598,1198,'a', 0);
+		p.move();
+		Assert.assertEquals(0, p.getX());
+		Assert.assertEquals(0, p.getY());
+		
+		int startDX = -100;
+		int endDX = 1000;
+		
+		int expectedX = 100;
+		int dExpectedX = -1;
+		for ( int dx = startDX; dx <= endDX; ++dx) {
+			p = new Particle(0,0,dx,0,'a', 0);
+			p.move();
+			Assert.assertEquals(expectedX, p.getX());
+			
+			expectedX += dExpectedX;
+			if ( expectedX == Main.width ) {
+				expectedX = Main.width - 2;
+				dExpectedX = -1;
+			} else if ( expectedX == -1 ) {
+				expectedX = 1;
+				dExpectedX = 1;
+			}
+		}	
+	}
+	
+	@Test
+	public void testParticleStaticStupidMove() {
+
+		Particle p = new Particle(799,599,-800,-600,'a', 0);
+		p.stupidMove();
+		Assert.assertEquals(1, p.getX());
+		Assert.assertEquals(1, p.getY());
+		
+		p = new Particle(0,0,800,600,'a', 0);
+		p.stupidMove();
+		Assert.assertEquals(798, p.getX());
+		Assert.assertEquals(598, p.getY());
+		
+		p = new Particle(799,599,1598,1198,'a', 0);
+		p.stupidMove();
+		Assert.assertEquals(799, p.getX());
+		Assert.assertEquals(599, p.getY());
+		
+		p = new Particle(0,0,1598,1198,'a', 0);
+		p.stupidMove();
+		Assert.assertEquals(0, p.getX());
+		Assert.assertEquals(0, p.getY());
+		
+		int startDX = -100;
+		int endDX = 1000;
+		
+		int expectedX = 100;
+		int dExpectedX = -1;
+		for ( int dx = startDX; dx <= endDX; ++dx) {
+			p = new Particle(0,0,dx,0,'a', 0);
+			p.stupidMove();
+			Assert.assertEquals(expectedX, p.getX());
+			
+			expectedX += dExpectedX;
+			if ( expectedX == Main.width ) {
+				expectedX = Main.width - 2;
+				dExpectedX = -1;
+			} else if ( expectedX == -1 ) {
+				expectedX = 1;
+				dExpectedX = 1;
+			}
+		}		
+	}
+	
+	@Test
+	public void testParticleStupidMoveVsMove() {
+				
+		for (int i = 0; i < 1000; i++ ) {
+			Particle p = new Particle(i);
+			p.setDx((int) (Math.random() * 10000 - 5000));
+			p.setDy((int) (Math.random() * 10000 - 5000));
+			Particle q = new Particle(p);
+			for ( int j = 0; j < 100; j++ ) {
+				p.stupidMove();
+				q.move();
+				Assert.assertEquals(p.getX(), q.getX());
+				Assert.assertEquals(p.getY(), q.getY());
 			}
 		}
 	}
