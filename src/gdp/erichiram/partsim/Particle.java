b0a34502067ae6253c2cea2f17b9d632e05e227f
hiramvanpaassen <hmvp@users.noreply.github.com>
2009-05-22 09:50:57 +0000

    only one move in particle and some minor changes in particle

--- src/gdp/erichiram/partsim/Particle.java
+++ src/gdp/erichiram/partsim/Particle.java
@@ -4,71 +4,77 @@
 
 package gdp.erichiram.partsim;
 
-
 /**
- * Particle is the class that represents a particle in the simulation.
- * it is very simple. 
- * It can be accessed concurrently by the animation threads that move it
- * and by the Gui for rendering.
- * the funny thing is that although none of the fields need the synchronized statements
- * (all read and writes to volatile fields or simple types (except long and double) are atomic)
- * we do need the synchronization to keep the particle from displaying strange
- * e.g. gui shows old X with new Y 
+ * Particle is the class that represents a particle in the simulation. it is
+ * very simple. It can be accessed concurrently by the animation threads that
+ * move it and by the Gui for rendering. the funny thing is that although none
+ * of the fields need the synchronized statements (all read and writes to
+ * volatile fields or simple types (except long and double) are atomic) we do
+ * need the synchronization to keep the particle from displaying strange e.g.
+ * gui shows old X with new Y
  */
 public class Particle {
 
 	/**
 	 * private vars of particle
 	 */
-	
+
 	/**
 	 * the x coordinate
 	 */
 	private volatile int x;
-	
+
 	/**
 	 * the y coordinate
 	 */
 	private volatile int y;
-	
+
 	/**
 	 * the speed in x direction (horizontal)
 	 */
 	private volatile int dx;
-	
+
 	/**
 	 * the speed in y direction (vertical)
 	 */
 	private volatile int dy;
-	
+
 	/**
 	 * the not so unique name of the particle
 	 */
 	private final char name;
-	
+
 	/**
 	 * the round the particle is in atm.
 	 */
 	private volatile int round;
-	
+
 	/**
 	 * the id of the last thread that has moved this particle
 	 */
 	private volatile long threadId;
-	
+
 	/**
-	 * boolean that tells if the particle is pending for removal and thus should be removed
+	 * boolean that tells if the particle is pending for removal and thus should
+	 * be removed
 	 */
 	private volatile boolean dead = false;
-	
+
 	/**
 	 * create particle with every parameter possible
-	 * @param x coordinate
-	 * @param y coordinate
-	 * @param dx the speed in horizontal direction
-	 * @param dy the speed in vertical direction
-	 * @param name of the particle
-	 * @param round the particle starts in
+	 * 
+	 * @param x
+	 *            coordinate
+	 * @param y
+	 *            coordinate
+	 * @param dx
+	 *            the speed in horizontal direction
+	 * @param dy
+	 *            the speed in vertical direction
+	 * @param name
+	 *            of the particle
+	 * @param round
+	 *            the particle starts in
 	 */
 	public Particle(int x, int y, int dx, int dy, char name, int round) {
 		this.x = x;
@@ -76,14 +82,16 @@ public class Particle {
 		this.dx = dx;
 		this.dy = dy;
 		this.name = name;
-		
+
 		this.threadId = 0;
 		this.round = round;
 	}
-	
+
 	/**
 	 * create a particle form another particle
-	 * @param other particle
+	 * 
+	 * @param other
+	 *            particle
 	 */
 	public Particle(Particle other) {
 		this.x = other.x;
@@ -91,14 +99,16 @@ public class Particle {
 		this.dx = other.dx;
 		this.dy = other.dy;
 		this.name = other.name;
-		
+
 		this.threadId = 0;
 		this.round = other.round;
 	}
-	
+
 	/**
 	 * create a random particle
-	 * @param round the particle is starting in
+	 * 
+	 * @param round
+	 *            the particle is starting in
 	 */
 	public Particle(int round) {
 		x = (int) (Math.random() * Main.width);
@@ -108,221 +118,129 @@ public class Particle {
 		// maximum value = 799
 		dx = (int) (Math.random() * (Main.width * 2 - 1)) - Main.width + 1;
 		dy = (int) (Math.random() * (Main.height * 2 - 1)) - Main.height + 1;
-		
-		if ( Math.random() > 0.5 ) {
+
+		if (Math.random() > 0.5) {
 			name = (char) ('a' + Math.random() * 26);
 		} else {
 			name = (char) ('A' + Math.random() * 26);
 		}
-		
+
 		this.round = round;
-		
+
 	}
-		
+
 	/**
-	 * the move of this particle, 
-	 * this method updates almost every bit of this particle
-	 * some heavy math is going on here ;-)
+	 * the move of this particle, this method updates almost every bit of this
+	 * particle some heavy math is going on here ;-)
 	 */
 	public synchronized void move() {
 
 		Main.debug("Moving particle " + this);
 		
-		////// change x and dx
-		x += dx;		
-		
-		// get "remainder" of x (rx) and the "number of bounces" (nx) using %
-		int rx = x % (Main.width - 1);
-		int nx = (x - rx) / (Main.width - 1);
-		
-		if ( x < 0 ) {
-			dx = Math.abs(dx);
-		}
-		
-		x = Math.abs(rx);
-		
-		// if nx is odd
-		if ( (nx & 1) == 1 ) {
-			// the direction is flipped and the position is mirrored
-			x = (Main.width - 1) - x;
-			dx = -dx;
-		}
+		//work on local variables this way we dont need to sync getX and getY
+		//unless ofcourse you want both from the same round but then its your duty to call both within a synced block
+		int x = this.x;
+		int y = this.y;
+		int dx = this.dx;
+		int dy = this.dy;
 
-		////// change y and dy
-		// (should be equivalent, mutatis mutandi, to the code block above)
-		y += dy;
-		
-		// get "remainder" of y (ry) and the "number of bounces" (ny) using % 
-		int ry = y % (Main.height - 1);	
-		int ny = (y - ry) / (Main.height - 1);
+		// //// change x and dx
+		x += dx;
 
-		if ( y < 0 ) {
-			dy = Math.abs(dy);
-		}
-		
-		y = Math.abs(ry);
-		
-		// if ny is odd
-		if ( (ny & 1) == 1 ) {
-			// the direction is flipped and the position is mirrored
-			y = (Main.height - 1) - y;			
-			dy = -dy;
-		}
-	
-		++round;
-		
-		threadId = Thread.currentThread().getId();
-	}
-	
-	/**
-	 * another {@link Particle#move() move} operation 
-	 * this one uses a loop to calculate
-	 * @see Particle#move() move
-	 */
-	public synchronized void stupidMove() {
+		if (x < 0 || x >= Main.width) {
 
-		Main.debug("sMoving particle " + this);
-		
-		////// change x and dx
-		int rdx = dx;
-		
-		do {
-			x += rdx;
-			
-			if ( x < 0 ) {
-				rdx = -x + 1;
-				x = -1;
-				dx = -dx;
-			} else if ( x >= Main.width ) {
-				rdx = - (x - (Main.width - 2));
-				x = Main.width;
-				dx = -dx;
-			}
-			
-		} while ( x < 0 || x >= Main.width );
-
-		////// change y and dy
-		int rdy = dy;
-		
-		do {
-			y += rdy;
-			
-			if ( y < 0 ) {
-				rdy = -y + 1;
-				y = -1;
-				dy = -dy;
-			} else if ( y >= Main.height ) {
-				rdy = - (y - (Main.height - 2));
-				y = Main.height;
-				dy = -dy;
-			}
-			
-		} while ( y < 0 || y >= Main.height );
-	
-		++round;
-		
-		threadId = Thread.currentThread().getId();
-	}
-	
-	/**
-	 * another move operation
-	 * this one is very similar to {@link Particle#move() move} 
-	 * but uses an extra test to check if all the heavy math is needed
-	 * @see Particle#move() move
-	 */
-	public synchronized void smartMove() {
-
-		Main.debug("Moving particle " + this);
-		
-		////// change x and dx
-		x += dx;		
-		
-		if ( x < 0 || x >= Main.width ) {
-			
-			// get "remainder" of x (rx) and the "number of bounces" (nx) using %
+			// get "remainder" of x (rx) and the "number of bounces" (nx) using
+			// %
 			int rx = x % (Main.width - 1);
 			int nx = (x - rx) / (Main.width - 1);
-			
-			if ( x < 0 ) {
+
+			if (x < 0) {
 				dx = Math.abs(dx);
 			}
-			
+
 			x = Math.abs(rx);
-			
+
 			// if nx is odd
-			if ( (nx & 1) == 1 ) {
+			if ((nx & 1) == 1) {
 				// the direction is flipped and the position is mirrored
 				x = (Main.width - 1) - x;
 				dx = -dx;
 			}
 		}
-		
-		////// change y and dy
+
+		// //// change y and dy
 		// (should be equivalent, mutatis mutandi, to the code block above)
 		y += dy;
-		
-		if ( y < 0 || y >= Main.height ) {
-				
-			// get "remainder" of y (ry) and the "number of bounces" (ny) using % 
-			int ry = y % (Main.height - 1);	
+
+		if (y < 0 || y >= Main.height) {
+
+			// get "remainder" of y (ry) and the "number of bounces" (ny) using
+			// %
+			int ry = y % (Main.height - 1);
 			int ny = (y - ry) / (Main.height - 1);
-	
-			if ( y < 0 ) {
+
+			if (y < 0) {
 				dy = Math.abs(dy);
 			}
-			
+
 			y = Math.abs(ry);
-			
+
 			// if ny is odd
-			if ( (ny & 1) == 1 ) {
+			if ((ny & 1) == 1) {
 				// the direction is flipped and the position is mirrored
-				y = (Main.height - 1) - y;			
+				y = (Main.height - 1) - y;
 				dy = -dy;
 			}
 		}
-	
-		++round;
+
+		this.x = x;
+		this.y = y;
+		this.dx = dx;
+		this.dy = dy;
 		
+		++round;
+
 		threadId = Thread.currentThread().getId();
 	}
-	
-	public synchronized int getX() {
+
+	public int getX() {
 		return x;
 	}
 
-	public synchronized int getY() {
+	public int getY() {
 		return y;
 	}
 
-	public synchronized void setDx(int dx) {
+	public void setDx(int dx) {
 		this.dx = dx;
 	}
-	
-	public synchronized int getDx() {
+
+	public int getDx() {
 		return dx;
 	}
-	
-	public synchronized void setDy(int dy) {
+
+	public void setDy(int dy) {
 		this.dy = dy;
 	}
-	
-	public synchronized int getDy() {
+
+	public int getDy() {
 		return dy;
 	}
 
 	public char getName() {
 		return name;
 	}
-	
+
 	public synchronized long getThreadId() {
 		return threadId;
 	}
-	
+
 	public synchronized String toString() {
-		return "\""+ name + "\"@(" + x + "," + y + ")+(" + dx + "," + dy + ")/" + round;
+		return "\"" + name + "\"@(" + x + "," + y + ")+(" + dx + "," + dy
+				+ ")/" + round;
 	}
 
-	public synchronized int getRound() {
+	public int getRound() {
 		return round;
 	}
 
@@ -332,13 +250,13 @@ public class Particle {
 	public void die() {
 		dead = true;
 	}
-	
+
 	/**
 	 * check if particle is not pending for removal
+	 * 
 	 * @return true if the particle is alive and needs to be processed
 	 */
-	public boolean process()
-	{
+	public boolean process() {
 		return !dead;
 	}
 }
--- src/gdp/erichiram/partsim/test/ParticleTest.java
+++ src/gdp/erichiram/partsim/test/ParticleTest.java
@@ -38,32 +38,6 @@ public class ParticleTest {
 	}
 	
 	@Test
-	public void testParticleStupidMove() {
-		
-		for (int i = 0; i < 200; i++ ) {
-			Particle p = new Particle(i);
-			for ( int j = 0; j < 10000; j++ ) {
-				p.stupidMove();
-				Assert.assertTrue(p.getX() >= 0 && p.getX() < 800 );
-				Assert.assertTrue(p.getY() >= 0 && p.getY() < 600 );
-			}
-		}
-	}
-	
-	@Test
-	public void testParticleSmartMove() {
-		
-		for (int i = 0; i < 200; i++ ) {
-			Particle p = new Particle(i);
-			for ( int j = 0; j < 10000; j++ ) {
-				p.smartMove();
-				Assert.assertTrue(p.getX() >= 0 && p.getX() < 800 );
-				Assert.assertTrue(p.getY() >= 0 && p.getY() < 600 );
-			}
-		}
-	}
-	
-	@Test
 	public void testParticleBigDMove() {
 		
 		for (int i = 0; i < 200; i++ ) {
@@ -72,36 +46,6 @@ public class ParticleTest {
 			p.setDy((int) (Math.random() * 10000 - 5000));
 			for ( int j = 0; j < 10000; j++ ) {
 				p.move();
-				Assert.assertTrue(p.getX() >= 0 && p.getX() < 800 );
-				Assert.assertTrue(p.getY() >= 0 && p.getY() < 600 );
-			}
-		}
-	}
-	
-	@Test
-	public void testParticleBigDStupidMove() {
-		
-		for (int i = 0; i < 200; i++ ) {
-			Particle p = new Particle(i);
-			p.setDx((int) (Math.random() * 10000 - 5000));
-			p.setDy((int) (Math.random() * 10000 - 5000));
-			for ( int j = 0; j < 10000; j++ ) {
-				p.stupidMove();
-				Assert.assertTrue(p.getX() >= 0 && p.getX() < 800 );
-				Assert.assertTrue(p.getY() >= 0 && p.getY() < 600 );
-			}
-		}
-	}
-	
-	@Test
-	public void testParticleBigDSmartMove() {
-		
-		for (int i = 0; i < 200; i++ ) {
-			Particle p = new Particle(i);
-			p.setDx((int) (Math.random() * 10000 - 5000));
-			p.setDy((int) (Math.random() * 10000 - 5000));
-			for ( int j = 0; j < 10000; j++ ) {
-				p.smartMove();
 				Assert.assertTrue(p.getX() >= 0 && p.getX() < 800 );
 				Assert.assertTrue(p.getY() >= 0 && p.getY() < 600 );
 			}
@@ -151,82 +95,5 @@ public class ParticleTest {
 			}
 		}	
 	}
-	
-	@Test
-	public void testParticleStaticStupidMove() {
 
-		Particle p = new Particle(799,599,-800,-600,'a', 0);
-		p.stupidMove();
-		Assert.assertEquals(1, p.getX());
-		Assert.assertEquals(1, p.getY());
-		
-		p = new Particle(0,0,800,600,'a', 0);
-		p.stupidMove();
-		Assert.assertEquals(798, p.getX());
-		Assert.assertEquals(598, p.getY());
-		
-		p = new Particle(799,599,1598,1198,'a', 0);
-		p.stupidMove();
-		Assert.assertEquals(799, p.getX());
-		Assert.assertEquals(599, p.getY());
-		
-		p = new Particle(0,0,1598,1198,'a', 0);
-		p.stupidMove();
-		Assert.assertEquals(0, p.getX());
-		Assert.assertEquals(0, p.getY());
-		
-		int startDX = -100;
-		int endDX = 1000;
-		
-		int expectedX = 100;
-		int dExpectedX = -1;
-		for ( int dx = startDX; dx <= endDX; ++dx) {
-			p = new Particle(0,0,dx,0,'a', 0);
-			p.stupidMove();
-			Assert.assertEquals(expectedX, p.getX());
-			
-			expectedX += dExpectedX;
-			if ( expectedX == Main.width ) {
-				expectedX = Main.width - 2;
-				dExpectedX = -1;
-			} else if ( expectedX == -1 ) {
-				expectedX = 1;
-				dExpectedX = 1;
-			}
-		}		
-	}
-	
-	@Test
-	public void testParticleStupidMoveVsMove() {
-				
-		for (int i = 0; i < 1000; i++ ) {
-			Particle p = new Particle(i);
-			p.setDx((int) (Math.random() * 10000 - 5000));
-			p.setDy((int) (Math.random() * 10000 - 5000));
-			Particle q = new Particle(p);
-			for ( int j = 0; j < 1000; j++ ) {
-				p.stupidMove();
-				q.move();
-				Assert.assertEquals(p.getX(), q.getX());
-				Assert.assertEquals(p.getY(), q.getY());
-			}
-		}
-	}
-	
-	@Test
-	public void testParticleStupidMoveVsSmartMove() {
-				
-		for (int i = 0; i < 1000; i++ ) {
-			Particle p = new Particle(i);
-			p.setDx((int) (Math.random() * 10000 - 5000));
-			p.setDy((int) (Math.random() * 10000 - 5000));
-			Particle q = new Particle(p);
-			for ( int j = 0; j < 1000; j++ ) {
-				p.stupidMove();
-				q.smartMove();
-				Assert.assertEquals(p.getX(), q.getX());
-				Assert.assertEquals(p.getY(), q.getY());
-			}
-		}
-	}
 }
