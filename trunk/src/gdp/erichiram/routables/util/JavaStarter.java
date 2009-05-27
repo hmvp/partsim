eb922f1eaef9ad6de9a98606f1f6dce91ae796b3
hiramvanpaassen <hiramvanpaassen@364f37a4-301b-11de-b3e3-fb1b389a3697>
2009-05-27 09:20:25 +0000

    begin routables

---
+++ trunk/src/gdp/erichiram/routables/util/JavaStarter.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package gdp.erichiram.routables.util;
+
+/**
+ * @author hiram, eric
+ *
+ */
+public class JavaStarter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
