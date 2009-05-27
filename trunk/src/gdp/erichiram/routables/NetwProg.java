1e0cf05c0b1037ca7580a21a1114ff91202c76f9
eric.broersma <eric.broersma@364f37a4-301b-11de-b3e3-fb1b389a3697>
2009-05-27 10:53:29 +0000

    blah

--- trunk/src/gdp/erichiram/routables/NetwProg.java
+++ trunk/src/gdp/erichiram/routables/NetwProg.java
@@ -1,13 +1,44 @@
 package gdp.erichiram.routables;
 
+import java.util.Collection;
+
+import gdp.erichiram.routables.Gui;
+
+import javax.swing.SwingUtilities;
+
 public class NetwProg {
 
+	RoutingTable table;
+	
+	int port;
+	
+	Collection<Integer> neighbours;
+	
+	Collection<SocketHandler> socketHandlers;
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		NetwProg netwProg = new NetwProg();
+		netwProg.initialize();
+	}
 
+	private void initialize() {		
+		// create table
+		table.setNeighbour(10, 20);
+		table.getNeighbour(20);
+		
+		// create gui
+		SwingUtilities.invokeLater(new Gui());
+		
+		// initialize threads for sockets
+		for ( int neighbour : neighbours ) {
+			SocketHandler socketHandler = new SocketHandler(Math.min(port, neighbour));
+			socketHandlers.add(socketHandler);
+			socketHandler.start();
+		}
+				
 	}
 
 }
---
+++ trunk/src/gdp/erichiram/routables/SocketHandler.java
@@ -0,0 +1,17 @@
+package gdp.erichiram.routables;
+
+public class SocketHandler extends Thread {
+
+	int port;
+	
+	public SocketHandler(int port) {
+		this.port = port;
+	}
+	
+	@Override
+	public void run() {
+	
+	}
+
+	
+}
