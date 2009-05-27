1be6e7d1797427b18a59b2b00910e2bc4088b6ec
hiramvanpaassen <hiramvanpaassen@364f37a4-301b-11de-b3e3-fb1b389a3697>
2009-05-27 10:30:57 +0000

    commit javastarter

---
+++ trunk/src/gdp/erichiram/routables/NetwProg.java
@@ -0,0 +1,13 @@
+package gdp.erichiram.routables;
+
+public class NetwProg {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- trunk/src/gdp/erichiram/routables/util/JavaStarter.java
+++ trunk/src/gdp/erichiram/routables/util/JavaStarter.java
@@ -3,6 +3,14 @@
  */
 package gdp.erichiram.routables.util;
 
+import gdp.erichiram.routables.NetwProg;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+
 /**
  * @author hiram, eric
  *
@@ -12,9 +20,67 @@ public class JavaStarter {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	
+	public static void main(String[] args) {		
+		new JavaStarter();
 	}
 
+	private Map<Integer,Set<Tuple>> m;
+	
+	private void putMap(int port, Tuple... data)
+	{
+		m.put(port, new HashSet<Tuple>(Arrays.asList(data)));
+	}
+	
+	public JavaStarter()
+	{
+		putMap(1100, new Tuple(1101,4), new Tuple(1102,3), new Tuple(1103,7));
+		putMap(1101, new Tuple(1100,4), new Tuple(1102,3), new Tuple(1103,7));
+		putMap(1102, new Tuple(1101,4), new Tuple(1100,3), new Tuple(1103,7));
+		putMap(1103, new Tuple(1101,4), new Tuple(1100,3), new Tuple(1100,7));
+
+		
+		for(int s : m.keySet())
+		{
+			startProcess(s, m.get(s));
+		}
+	}
+	
+	private Thread startProcess(final Integer port, final Set<Tuple> d)
+	{
+		Thread t = new Thread(){
+
+			public void run() {
+				String[] args = new String[(d.size() * 2) + 1];
+				args[0] = port.toString();
+				int i = 1;
+				for (Tuple p : d)
+				{
+					args[i++] = p.port;
+					args[i++] = p.w;
+				}
+				
+				NetwProg.main(args);
+			}
+			
+		};
+		
+		t.start();
+		return t;
+	}
 }
+
+class Tuple {
+	public String port;
+	public String w;
+	
+	public Tuple(Integer port, Integer w){
+		if(port <= 1100 || port > 1120 ||w < 1 || w > 1000)
+			throw new RuntimeException("AArggh input wrong!");
+		
+		this.port = port.toString();
+		this.w = w.toString();
+	}
+}
+
