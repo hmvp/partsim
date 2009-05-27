26b1b29852f3fcc12784ff5384f98b96c59d7f5d
eric.broersma <eric.broersma@364f37a4-301b-11de-b3e3-fb1b389a3697>
2009-05-27 15:03:30 +0000

    Util.debug toegevoegd Message-objecten (Fail, MyDist, Repair) toegevoegd NetwProg uitgebreid RoutingTable iets mooier gemaakt SocketHandler zou nu moeten kunnen receiven

--- trunk/src/gdp/erichiram/routables/NetwProg.java
+++ trunk/src/gdp/erichiram/routables/NetwProg.java
@@ -1,6 +1,8 @@
 package gdp.erichiram.routables;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import gdp.erichiram.routables.Gui;
 
@@ -8,37 +10,61 @@ import javax.swing.SwingUtilities;
 
 public class NetwProg {
 
-	RoutingTable table;
-	
-	int port;
-	
-	Collection<Integer> neighbours;
-	
-	Collection<SocketHandler> socketHandlers;
-	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		// TODO: initialize neighbours
-		
-		NetwProg netwProg = new NetwProg();
-		netwProg.initialize();
+
+		// TODO: remove these sample values when the argument parser is in place
+		int argId = 1100;
+		int argNeighbours[] = { 1104, 1103 };
+		int argWeights[] = { 10, 5 };
+
+		// create the application
+		new NetwProg(argId, argNeighbours, argWeights);
 	}
 
-	private void initialize() {
-		// TODO: create table
-		// table.create hahah
-		
-		// create gui
+	private int id;
+	private Collection<Integer> neighbours;
+	private Map<Integer, SocketHandler> socketHandlers;
+	private RoutingTable routingTable;
+
+	public NetwProg(int argId, int[] argNeighbours, int[] argWeights) {
+
+		this.id = argId;
+
+		initializeNeighbours(argNeighbours, argWeights);
+		initializeRoutingTable();
+		initializeGui();
+		initializeSocketHandlers();
+	}
+
+	private void initializeNeighbours(int[] argNeighbours, int[] argWeights) {
+		// TODO: initialize neighbours
+		for (int neighbour : argNeighbours) {
+			neighbours.add(neighbour);
+		}
+	}
+
+	private void initializeRoutingTable() {
+		// TODO initialize routing table
+	}
+
+	private void initializeGui() {
+		// start a separate thread to create and run the gui
 		SwingUtilities.invokeLater(new Gui());
-		
+	}
+
+	private void initializeSocketHandlers() {
 		// initialize threads for sockets
-		for ( int neighbour : neighbours ) {
-			SocketHandler socketHandler = new SocketHandler(Math.min(port, neighbour));
-			socketHandlers.add(socketHandler);
+		socketHandlers = new HashMap<Integer, SocketHandler>();
+		for (int neighbour : neighbours) {
+			// TODO: this is not the way to decide the port number to use
+			int port = Math.min(id, neighbour);
+			
+			SocketHandler socketHandler = new SocketHandler(port, routingTable);
+			socketHandlers.put(neighbour, socketHandler);
 			socketHandler.start();
-		}				
+		}
 	}
 }
