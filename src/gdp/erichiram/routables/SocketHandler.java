81a18b69fe51f9387512e32cb0cfab42c3c65712
hiramvanpaassen <hmvp@users.noreply.github.com>
2009-05-30 21:06:30 +0000

    beetje gui and working socket code

--- src/gdp/erichiram/routables/SocketHandler.java
+++ src/gdp/erichiram/routables/SocketHandler.java
@@ -1,13 +1,14 @@
 package gdp.erichiram.routables;
 
+import gdp.erichiram.routables.message.Fail;
 import gdp.erichiram.routables.message.Message;
 import gdp.erichiram.routables.message.MyDist;
 import gdp.erichiram.routables.message.Repair;
-import gdp.erichiram.routables.message.Fail;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -25,11 +26,24 @@ public class SocketHandler extends Thread {
 
 		try {
 			// create a socket and connect it to the specified port on the loopback interface
-			socket = new Socket((String)null, port);
+			socket = new Socket(InetAddress.getLocalHost(), port);
 			in = new ObjectInputStream(socket.getInputStream());
 			out = new ObjectOutputStream(socket.getOutputStream());
 		} catch (UnknownHostException e) {
 			System.err.println("[" + port + "] Localhost is an unknown host: "	+ e.getLocalizedMessage());
+		} catch (IOException e) {
+			System.err.println("[" + port + "] Could not create socket or get inputstream from socket: " + e.getLocalizedMessage());
+		}
+	}
+
+	public SocketHandler(Socket socket, RoutingTable routingTable) {
+		this.socket = socket;
+		this.port = socket.getPort();
+		this.routingTable = routingTable;
+		
+		try {
+			in = new ObjectInputStream(socket.getInputStream());
+			out = new ObjectOutputStream(socket.getOutputStream());
 		} catch (IOException e) {
 			System.err.println("[" + port + "] Could not create socket or get inputstream from socket: " + e.getLocalizedMessage());
 		}
@@ -40,8 +54,7 @@ public class SocketHandler extends Thread {
 		while (true) {
 			try {
 				// read a message object from the input stream
-				Object message = in.readObject();
-				
+				Object message = in.readObject();				
 				// relay the message to the routing table
 				if (message instanceof MyDist) {
 					routingTable.receive((MyDist)message);
