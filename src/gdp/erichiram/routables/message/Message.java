16ad0f80f9a5d5ceb48c963a87d8410b80a77939
eric.broersma <eric.broersma@364f37a4-301b-11de-b3e3-fb1b389a3697>
2009-06-24 13:54:27 +0000

    Commented the message classes.

--- src/gdp/erichiram/routables/message/Message.java
+++ src/gdp/erichiram/routables/message/Message.java
@@ -2,6 +2,10 @@ package gdp.erichiram.routables.message;
 
 import java.io.Serializable;
 
+/**
+ * Encode a general message.
+ * @author eric, hiram
+ */
 public abstract class Message implements Serializable{
 	private static final long serialVersionUID = -7622270218128154257L;
 	
