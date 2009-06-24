0bebf3e4ed166f31f2056c37678e9f5b6be9cc71
eric.broersma <eric.broersma@364f37a4-301b-11de-b3e3-fb1b389a3697>
2009-06-24 13:40:06 +0000

    Comments for Channel and RoutingTableTableModel. setNiceLocation is only called when the GUI is started. die() renamed to close() in Channel

--- src/gdp/erichiram/routables/RoutingTableTableModel.java
+++ src/gdp/erichiram/routables/RoutingTableTableModel.java
@@ -8,14 +8,32 @@ import java.util.Observer;
 import javax.swing.SwingUtilities;
 import javax.swing.table.AbstractTableModel;
 
+/**
+ * This model is used to show the information from RoutingTable in the GUI.
+ * @author eric, hiram
+ *
+ */
 public class RoutingTableTableModel extends AbstractTableModel implements Observer{
 
 	private static final long serialVersionUID = 3441755023701740847L;
 
+	/**
+	 * List of column names.
+	 */
 	private final String[] columnNames = { "Node", "Neighbour to use", "Length of path" };
+	
+	/**
+	 * Cache for nodes data.
+	 */
 	private Map<Integer, Integer[]> nodes = new HashMap<Integer, Integer[]>(20);
 
+	/**
+	 * Constructor for RoutingTableTablemodel.
+	 * @param netwProg Reference to the main program.
+	 */
 	public RoutingTableTableModel(NetwProg netwProg) {
+		
+		// Load initial nodes data.
 		Integer[][] data = netwProg.routingTable.getNodesData();
 		for(Integer[] d : data)
 		{
@@ -23,18 +41,30 @@ public class RoutingTableTableModel extends AbstractTableModel implements Observ
 		}
 	}
 
+	/**
+	 * @see AbstractTableModel#getColumnName(int)
+	 */
 	public String getColumnName(int col) {
 		return columnNames[col].toString();
 	}
 
+	/**
+	 * @see AbstractTableModel#getRowCount()
+	 */
 	public int getRowCount() {
 		return nodes.size();
 	}
 
+	/**
+	 * @see AbstractTableModel#getColumnCount()
+	 */
 	public int getColumnCount() {
 		return columnNames.length;
 	}
 
+	/**
+	 * @see AbstractTableModel#getValueAt(int, int)
+	 */
 	public Object getValueAt(int row, int col) {
 		
 		if (	( col == 1  && nodes.get(nodes.keySet().toArray(new Integer[0])[row])[col] == RoutingTable.UNDEF_ID)
@@ -44,11 +74,17 @@ public class RoutingTableTableModel extends AbstractTableModel implements Observ
 			return nodes.get(nodes.keySet().toArray(new Integer[0])[row])[col];
 		}
 	}
-
+	
+	/**
+	 * @see AbstractTableModel#isCellEditable(int, int)
+	 */
 	public boolean isCellEditable(int row, int col) {
 		return false;
 	}
-
+	
+	/**
+	 * @see Observer#update(Observable, Object)
+	 */
 	public void update(final Observable observable, final Object message) {
 		SwingUtilities.invokeLater(new Runnable(){
 			public void run(){
